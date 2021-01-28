9205f83c8c35e75b208a63c97e93df01b648bd7b
Yauheniy <buchenkov@gmail.com>
2021-01-28 16:40:31 +0300

    выполнил: 5, 7

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,66 @@
 "use strict";
 
+// задача 5
+const fiveTask = {
+    name: "Yauhen",
+    secondName: "Buch",
+    age: "101",
+};
+
 let inputMain = document.getElementById('buttonSearch');
 inputMain.addEventListener('click', getValue);
 
 function getValue() {
     let getZnach = document.getElementById('idInput').value;
 
-    //console.log(typeof(getZnach));
-    //alert(`${getZnach}`);
-
     if (getZnach == "google" ) {
-        console.log("Yandex круче. Но это не точно");
-        setTimeout(() => alert('Yandex круче. Но это не точно'), 3000);  // функция с задержкой в 3 секунды https://learn.javascript.ru/settimeout-setinterval
+        console.log(fiveTask.name, 'Yandex круче. Но это не точно');
+        alert(`${fiveTask.name} Yandex круче. Но это не точно`);
+/* 
+        setTimeout(
+            () => alert(`${fiveTask.name} Yandex круче. Но это не точно`), 
+        3000);
+ */
     } else {
-        console.log(getZnach);
-        alert(`${getZnach}`);
-        //setTimeout(() => alert(`${getZnach}`), 3000);
+        console.log(fiveTask.name, getZnach);
+        alert(`${fiveTask.name} ${getZnach}`);
+/* 
+        setTimeout(
+            () => alert(`${fiveTask.name} ${getZnach}`),
+        3000); 
+ */
     }
 } 
+
+/* НАДО С ПОМОЩЬЮ ЦИКЛА FOR
+let arr = [33, 99, 3, 77, 55, 100, 433, 250];
+let min = arr[0];
+let max = arr[0];
+
+arr.forEach(function (elem, index) {
+
+    if(index > 0 ){
+        if(max < elem){
+            max = elem;
+        } else if (min > elem) {
+            min = elem;
+        }
+    }
+});
+console.log(min,max);
+*/
+let arr = [33, 99, 3, 77, 55, 100, 433, 250];
+let maxValue = arr[0];
+let minValue = arr[0];
+
+for(let i = 0; i < arr.length; i++) {
+   let currentValue = arr[i];
+   if(currentValue > maxValue) {
+      maxValue = currentValue;
+   } else if (currentValue < minValue) {
+      minValue = currentValue;
+   }
+}
+console.log(minValue, maxValue); 
+// document.getElementById('minValue').innerHTML = minValue;
+// document.getElementById('maxValue').innerHTML = maxValue;
